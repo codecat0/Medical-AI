a708604f1797fdd3b12622cab6a6a6e71764b65a
codecat <2049413359@qq.com>
2024-06-11 16:05:32 +0800

    添加损失函数

--- Medical3DSeg/models/losses/__init__.py
+++ Medical3DSeg/models/losses/__init__.py
@@ -6,3 +6,6 @@
 @Date   :2024/6/11 14:54
 """
 from .loss_utils import class_weights, flatten
+from .dice_loss import DiceLoss
+from .cross_entropy_loss import CrossEntropyLoss
+from .mixed_loss import MixedLoss
